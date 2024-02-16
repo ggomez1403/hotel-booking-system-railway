a303965a76c3407a0961748bd43ca71ba13448f4
Geraldine Gómez <geralgomez1403@gmail.com>
2024-02-15 19:45:02 -0500

    Cors Config updated

--- src/main/java/com/ggomezr/bookingsystem/application/config/CorsConfig.java
+++ src/main/java/com/ggomezr/bookingsystem/application/config/CorsConfig.java
@@ -20,7 +20,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
-        configuration.addAllowedOrigin("https://ggomez1403.github.io/hotel-booking-angular/");
+        configuration.addAllowedOrigin("https://ggomez1403.github.io/");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", configuration);
